dd9db896173b1b0a5406771a9ea8ce6e87123ea5
Benito-sba <nitoacevedo46@gmail.com>
2026-02-25 17:57:29 -0600

    Add files via upload

---
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
@@ -0,0 +1,13 @@
+// Importar paquetes instalados desde NPM
+import generateName from 'sillyname';
+import { randomSuperhero } from 'superheroes';
+
+
+// ------------------------------
+// superhéroe aleatorio
+// ------------------------------
+
+
+const hero = randomSuperhero();
+
+console.log(`Nombre de superhéroe: ${hero}`);
