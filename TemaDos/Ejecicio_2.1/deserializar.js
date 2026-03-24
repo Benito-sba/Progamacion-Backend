aa15f3ce3739d958b1eddce76200a827ca8161d6
Benito-sba <nitoacevedo46@gmail.com>
2026-03-23 23:44:38 -0500

    Update deserializar.js

--- TemaDos/Ejecicio_2.1/deserializar.js
+++ TemaDos/Ejecicio_2.1/deserializar.js
@@ -1,3 +1,6 @@
+//AA 2.1 API Reporte practico 
+//BENITO SANTIAGO BALAM ACEVEDO
+
 // Cadena JSON (nota: las claves van entre comillas)
 const jsonString = '{"nombre":"Taco de Pollo","ingredientes":{"proteina":"Pollo","salsa":"Salsa Verde"}}';
 
