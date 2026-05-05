17e3f197e8f5c5976ef485afd493de217328bbaf
Benito-sba <nitoacevedo46@gmail.com>
2026-05-05 01:16:18 -0500

    Proyecto CRUD Finalizado

--- tema 3/AA 3.1_Proyecto_CRUD_API/index.js
+++ tema 3/AA 3.1_Proyecto_CRUD_API/index.js
@@ -31,6 +31,81 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a mi API CRUD');
 });
 
+
+// seccion 5
+
+app.get('/usuarios', async (req, res)=>{
+    try{
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    }catch(error){
+        console.error("Error al obtener los usuarios:", error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+});
+
+// seccion 6
+
+app.get('/usuario/:id', async (req, res) => {
+    try {
+
+        const {id} = req.params; 
+        const usuario = await Usuario.findById(id)
+        res.status(200).json(usuario);
+
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+        res.status(500).json({ error: 'Error al obtener el usuario' });
+    }
+});
+
+// seccion 7
+
+
+app.put('/usuario/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+        const usuarioActualizado = await Usuario.findById(id);
+        res.status(200).json(usuarioActualizado);
+        console.log("Usuario actualizado con éxito:", usuarioActualizado);
+
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+        res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
+});
+
+
+
+// Sección 8
+app.delete('/usuario/:id', async (req, res) => {
+    try {
+        const { id } = req.params; 
+        
+    
+        const usuario = await Usuario.findByIdAndDelete(id);
+
+     
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+     
+        res.status(200).json({ message: 'Usuario eliminado' });
+
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+        res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
+});
+
+
 app.listen(puerto, () => {
     console.log(`Servidor escuchando en http://localhost:${puerto}`);
 });
